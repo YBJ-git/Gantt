c2d6392690df581c5df0f0d5f989d0b181acf4c9
YBJ-git <excel1516@naver.com>
2025-05-20 09:10:29 +0900

    Netlify 404 에러 해결을 위한 설정 추가.V8

---
+++ frontend/src/components/feature/Reports/index.js
@@ -0,0 +1,18 @@
+import LoadTrendsChart from './LoadTrendsChart';
+import ResourceUtilizationChart from './ResourceUtilizationChart';
+import TaskCompletionChart from './TaskCompletionChart';
+import CostAnalysisChart from './CostAnalysisChart';
+
+export {
+  LoadTrendsChart,
+  ResourceUtilizationChart,
+  TaskCompletionChart,
+  CostAnalysisChart
+};
+
+export default {
+  LoadTrendsChart,
+  ResourceUtilizationChart,
+  TaskCompletionChart,
+  CostAnalysisChart
+};
