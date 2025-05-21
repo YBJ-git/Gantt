531da0c44abd7096e20802b641bbeae23ba8a91e
YBJ-git <excel1516@naver.com>
2025-05-21 15:06:44 +0900

    서브모듈 참조 문제 해결: Gantt 서브모듈 제거

--- Gantt/frontend/src/components/feature/LoadOptimization/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoadHeatmap } from './LoadHeatmap';
-export { default as LoadGauge } from './LoadGauge';
-export { default as LoadOptimizationRecommendations } from './LoadOptimizationRecommendations';
-export { default as AutoTaskDistribution } from './AutoTaskDistribution';
--- Gantt/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
-  coverageDirectory: '<rootDir>/coverage',
-  testTimeout: 10000,
-  modulePathIgnorePatterns: ['node_modules'],
-  roots: ['<rootDir>/tests']
-};
